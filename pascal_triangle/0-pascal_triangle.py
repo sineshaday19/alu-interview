a17a17431d715c60a325b675af0b430c08886f28
Sine Shaday <s.sine@alustudent.com>
2024-03-12 21:27:44 +0000

    update

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,36 +1,18 @@
-Ascal_triangle.py
-"""A python script that
-doe the pascal triangle
-"""
+#!/usr/bin/python3
+"""Pascal Triangle"""
 
 
 def pascal_triangle(n):
-    """The function doing
-    pascal triangle it was beautiful
-    and technical to write
-    """
-    list_of_list = []
+    """Returns a list of lists of integers representing
+    the Pascal's triangle of n"""
     if n <= 0:
-        return list_of_list
-    else:
-        if n == 1:
-            list1 = [1]
-            list_of_list.append(list1)
-            return list_of_list
-        else:
-            for i in range(n):
-                if i == 0:
-                    list2 = [1]
-                    list_of_list.append(list2)
-                else:
-                    list3 = list_of_list[i - 1]
-                    list4 = []
-                    for j in range(i + 1):
-                        if j == 0:
-                            list4.append(list3[j] + 0)
-                        elif j > 0 and len(list3) != j:
-                            list4.append(list3[j - 1] + list3[j])
-                        else:
-                            list4.append(list3[j - 1] + 0)
-                    list_of_list.append(list4)
-    return list_of_list
+        return []
+    res = [[1]]
+
+    for i in range(n-1):
+        row = []
+        temp = [0] + res[-1] + [0]
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j+1])
+        res.append(row)
+    return res
